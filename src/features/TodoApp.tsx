615af1e97f0aef88a3ad81d46b792ec7528f8d13
Vitalii <vitalii.kostiura@gmail.com>
2023-03-01 19:17:15 +0200

    feat: using dispath to add new todo

--- src/features/TodoApp.tsx
+++ src/features/TodoApp.tsx
@@ -1,3 +1,36 @@
+import React from "react";
+import { useState } from "react";
+import { useAppDispatch } from "../app/hooks/useAppDispatch";
+import { store } from "../app/store";
+import { AddTodoForm } from "../components/AddTodoForm/AddTodoForm";
+import { addTodo, todosSelectors } from "./todos/TodosSlice";
+
+const columnData = ["ID", "TITLE", "DESCRIPRION", "STATUS"];
+
 export const TodoApp: React.FC = () => {
-	return <div></div>;
+	const todoList = todosSelectors.selectAll(store.getState());
+
+	const [, setTodods] = useState(todoList);
+
+	const dispatch = useAppDispatch();
+
+	const addNewTodo = (
+		titleData: string | number,
+		descriptionData: string | number
+	) => {
+		dispatch(
+			addTodo({
+				id: todoList.length + 1,
+				title: titleData,
+				description: descriptionData,
+				status: false,
+			})
+		);
+		setTodods(todosSelectors.selectAll(store.getState()));
+	};
+	return (
+		<div>
+			<AddTodoForm addNewTodo={addNewTodo} />
+		</div>
+	);
 };
